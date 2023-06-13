88832b890d2d88926cd6f55be246697bbd7fc16b
xGIANOx <studioenricogian@gmail.com>
2023-06-13 18:03:01 +0200

    [add] ProjectCard component

--- vite-boolfolio/src/App.vue
+++ vite-boolfolio/src/App.vue
@@ -1,60 +1,45 @@
 <script>
-import axios from "axios"
-  
-export default{
+import axios from "axios";
+import ProjectCard from "./components/ProjectCard.vue";
+
+export default {
   name: "App",
-  data(){
-    return{
-      base_URL: 'http://127.0.0.1:8000/',
-      projects_path: 'api/projects',
+  components: {
+    ProjectCard,
+  },
+  data() {
+    return {
+      base_URL: "http://127.0.0.1:8000/",
+      projects_path: "api/projects",
       loading: true,
       projects: [],
-    }
+    };
   },
-  methods:{
-    // getProjects(url){
-    //   axios
-    //   .get(url)
-    //   .then(response => {
-    //     this.projects= response.data;
-    //     console.log(this.projects)
-    //   })
-    // }
-  },
-  mounted(){
+  mounted() {
     const completeUrl = this.base_URL + this.projects_path;
-        axios.get(completeUrl)
-        .then(response => {
-            this.projects = response.data.projects.data;
-            console.log(this.projects);
-        })
-        .catch(error => {
-            console.error(error);
-        })
-  }
-}
-      
+    axios
+      .get(completeUrl)
+      .then((response) => {
+        this.projects = response.data.projects.data;
+        console.log(this.projects);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  },
+};
 </script>
 
 <template>
   <div>
-   
     <div class="container">
       <div class="row">
-        <div class="col g-4" v-for="project in projects">
-          <div class="card h-100">
-            <div class="card-header h-100">
-              {{ project.title }}
-            </div>
-            <img class="w-100" :src="project.img" :alt="project.title">
-          </div>
-        </div>
+        <ProjectCard />
       </div>
     </div>
-
   </div>
 </template>
 
-<style scoped lang="scss">
 
+<style scoped lang="scss">
 </style>
---
+++ vite-boolfolio/src/components/ProjectCard.vue
@@ -0,0 +1,56 @@
+<script>
+import axios from "axios";
+
+  export default {
+    name: "ProjectCard",
+    // props: {
+    //   project: {
+    //     type: Object,
+    //   },
+    // },
+    data() {
+    return {
+      base_URL: "http://127.0.0.1:8000/",
+      projects_path: "api/projects",
+      loading: true,
+      projects: [],
+    };
+  },
+  mounted() {
+    const completeUrl = this.base_URL + this.projects_path;
+    axios
+      .get(completeUrl)
+      .then((response) => {
+        this.projects = response.data.projects.data;
+        console.log(this.projects);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  },
+  };
+  </script>
+  
+  <style scoped lang="scss">
+  </style>
+
+
+<template>
+    <div>
+      <div class="container">
+        <div class="row">
+          <div class="col g-4" v-for="project in projects" :key="project.id">
+            <div class="card h-100">
+              <div class="card-header h-100">
+                {{ project.title }}
+              </div>
+              <img class="w-100" :src="project.img" :alt="project.title">
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+</template>
+
+
+
